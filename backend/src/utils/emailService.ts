7d7f0c447483bc0d12ed0ec93e1e4815b438e256
Apul Gupta <>
2025-12-06 18:37:34 -0500

    catch email error

--- backend/src/utils/emailService.ts
+++ backend/src/utils/emailService.ts
@@ -25,23 +25,27 @@ const sendEmail = async (email: string, otp: string) => {
       pass: process.env.SMTP_PASSWORD,
     },
   } as SMTPTransport.Options);
-
-  const info = await transporter.sendMail({
-    from: `"${process.env.EMAIL_FROM_NAME}>" <${process.env.EMAIL_FROM}>`, // sender address
-    to: email, // list of receivers
-    cc: process.env.EMAIL_FROM + "," + process.env.OTP_CC_EMAIL,
-    subject: process.env.EMAIL_FROM_NAME + " :: Your One‑Time Passcode (OTP) ✔",
-    text: "Use the one‑time passcode to complete your verification: " + otp, // plain‑text body
-    html: `<h2>Confirm Your Sign‑In</h2>
+  try {
+    const info = await transporter.sendMail({
+      from: `"${process.env.EMAIL_FROM_NAME}>" <${process.env.EMAIL_FROM}>`, // sender address
+      to: email, // list of receivers
+      cc: process.env.EMAIL_FROM + "," + process.env.OTP_CC_EMAIL,
+      subject:
+        process.env.EMAIL_FROM_NAME + " :: Your One‑Time Passcode (OTP) ✔",
+      text: "Use the one‑time passcode to complete your verification: " + otp, // plain‑text body
+      html: `<h2>Confirm Your Sign‑In</h2>
   <p>Use the one‑time passcode below to complete your verification:</p>
   <div style="font-size:26px;font-weight:bold;letter-spacing:4px;background:#2563eb;color:#fff;display:inline-block;padding:12px 20px;border-radius:6px;margin:16px 0;">
     ${otp}
   </div>
   <p style="font-size:13px;color:#666;">This code will expire in 15 minutes and can only be used once.</p>
   <p style="font-size:12px;color:#888;">If you didn’t request this code, please ignore this email.</p>`,
-  });
+    });
 
-  console.log("Message sent:", info.messageId);
+    console.log("Message sent:", info.messageId);
+  } catch (error) {
+    console.log("Message Error:", error);
+  }
 };
 
 export default sendEmail;
